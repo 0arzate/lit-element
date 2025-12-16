8b58cbe3cf5757be22c1ebe810177661cc79c192
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2025-12-15 20:20:56 -0600

    feat(pages): ✨ add page example

--- app/pages/home-page/home-page.js
+++ app/pages/home-page/home-page.js
@@ -1,22 +1,37 @@
 import { html, LitElement } from 'lit-element'
+import { styles } from './home-page.css'
 
 class HomePage extends LitElement {
+  constructor () {
+    super()
+    this.person = { name: 'Osvaldo' }
+  }
+
   static get is () {
     return 'home-page'
   }
 
-  static get properties () {
-    return {}
+  static get properties () { // oldValue !== newValue
+    return {
+      person: { type: Object }
+    }
   }
 
   static get styles () {
-    return []
+    return styles
+  }
+
+  changePersonName () {
+    this.person.name = 'Samuel'
   }
 
   render () {
     return html`
       <main>
-        <h1>${HomePage.is}</h1>
+        <h1>${this.person.name}</h1>
+        <button @click="${this.changePersonName}">Cambiar nombre</button>
+
+        <person-card .data="${{ name: 'Juan' }}" .isAlive="${true}" ></person-card>
       </main>
     `
   }
